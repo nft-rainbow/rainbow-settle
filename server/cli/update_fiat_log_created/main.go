29d0cfaeea704719d8842db42ec86910ddf2388b
dayong <wangdayong228@163.com>
2024-01-09 15:34:49 +0800

    add script for update fiat_logs created_at by cache_ids

---
+++ server/cli/update_fiat_log_created/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"time"
+
+	"github.com/nft-rainbow/conflux-gin-helper/logger"
+	"github.com/nft-rainbow/rainbow-settle/common/models"
+	"github.com/nft-rainbow/rainbow-settle/server/config"
+	"github.com/sirupsen/logrus"
+)
+
+func updateFlCreatedByCacheIds(start time.Time, end time.Time) error {
+	var fls []*models.FiatLog
+	if err := models.GetDB().Where("created_at>? and created_at<?", start, end).Find(&fls).Error; err != nil {
+		return err
+	}
+
+	for _, fl := range fls {
+		flcId := fl.CacheIds[len(fl.CacheIds)-1]
+		var flc models.FiatLogCache
+		if err := models.GetDB().First(&flc, flcId).Error; err != nil {
+			return err
+		}
+
+		if err := models.GetDB().Debug().
+			Model(&models.FiatLog{}).
+			Where("id=?", fl.ID).
+			Update("created_at", flc.CreatedAt).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func init() {
+	config.InitByFile("../../config.yaml")
+	models.ConnectDB(config.Get().Mysql)
+
+	logger.Init(config.Get().Log, "====== CLI: Update Fiatlog Created_at By Cache_Ids ========")
+}
+
+func main() {
+	err := updateFlCreatedByCacheIds(time.Date(2024, 1, 8, 0, 0, 0, 0, time.Local), time.Date(2024, 1, 9, 0, 0, 0, 0, time.Local))
+	logrus.WithError(err).Info("relate all refund sponsor fiatlogs completed")
+}
